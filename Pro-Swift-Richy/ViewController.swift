9fb0ac94e23c4a831358f7db347f4517e709749a
iMaricela <44278837+iMaricela@users.noreply.github.com>
2020-08-22 17:46:21 +0200

    Initial Commit

---
+++ Pro-Swift-Richy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pro-Swift-Richy
+//
+//  Created by Marzieh Sharafi on 8/22/20.
+//  Copyright © 2020 Marzieh Sharafi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
